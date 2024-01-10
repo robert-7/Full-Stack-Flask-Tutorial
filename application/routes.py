8243a48e2b699dfd6943c32d10f54d1b8f395be0
robert-7 <robert.lech123@gmail.com>
2024-01-09 21:27:51 -0500

    fix: updates for Ubuntu 22.04

--- application/routes.py
+++ application/routes.py
@@ -94,7 +94,7 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         user_id = User.objects.count()
-        app.logger.info("ROBER - %s - the number of users", user_id)
+        app.logger.info("ROBERT - %s - the number of users", user_id)
         user_id += 1
         email = form.email.data
         password = form.password.data
